5ab8f377d1cdaa8e60bc28ef7531a3cec5da11a6
julja83 <59063644+julja83@users.noreply.github.com>
2020-01-05 14:00:13 +0300

    Добавлена возможность запуска в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -2,7 +2,11 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+//import sun.plugin2.util.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,9 +16,24 @@ public class ApplicationManager {
   private NavigationHelper navigationHelper;
   private  ContactHelper contactHelper;
   private GroupHelper groupHelper;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void init() {
-  wd = new FirefoxDriver();
+    //String browser= BrowserType.FIREFOX;
+    if(browser.equals(BrowserType.FIREFOX))
+    { wd = new FirefoxDriver();
+    }
+    else if (browser.equals(BrowserType.CHROME)){
+      wd = new ChromeDriver();
+    }
+    else if (browser.equals(BrowserType.IEXPLORE)){
+      wd=new InternetExplorerDriver();
+    }
+
   wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
   wd.get("http://localhost/addressbook/");
     groupHelper = new GroupHelper(wd);
@@ -23,9 +42,6 @@ public class ApplicationManager {
     sessionHelper=new SessionHelper(wd);
     sessionHelper.login("admin", "secret");
   }
-
-
-
   public void logout() {
    wd.findElement(By.linkText("Logout")).click();
   }
@@ -34,9 +50,6 @@ public class ApplicationManager {
     logout();
     wd.quit();
   }
-
-
-
   public GroupHelper getGroupHelper() {
     return groupHelper;
   }
@@ -44,7 +57,6 @@ public class ApplicationManager {
   public ContactHelper getContactHelper() {
     return contactHelper;
   }
-
   public NavigationHelper getNavigationHelper() {
     return navigationHelper;
   }
