8caa7736c53d4f32de9fa3e17fe49ee845b77d29
julja83 <59063644+julja83@users.noreply.github.com>
2020-02-02 16:54:39 +0300

    Внедрение уникальных идентификаторов в тесты для групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -6,22 +6,20 @@ import org.openqa.selenium.WebElement;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class GroupHelper extends HelperBase {
-
   public GroupHelper(WebDriver wd) {
     super(wd);
   }
-
   public void returnToGroupPage() {
     click(By.linkText("group page"));
   }
-
   public void submitGroupCreation() {
     click(By.name("submit"));
   }
-
   public void fillGroupForm(GroupData groupData) {
     type(By.name("group_name"), groupData.getName());
     type(By.name("group_header"), groupData.getHeader());
@@ -31,20 +29,15 @@ public class GroupHelper extends HelperBase {
   public void initGroupCreation() {
     click(By.name("new"));
   }
-
   public void deleteSelectedGroups( ) {
-
     click(By.name("delete"));
   }
-
-  public void selectGroup(int index) {
-    wd.findElements(By.name("selected[]")).get(index).click();
+   public void selectGroupById(int id) {
+    wd.findElement(By.cssSelector("input[value='"+id+"'")).click();
   }
-
   public void initGroupModification() {
     click(By.name("edit"));
   }
-
   public void submitGroupModification() {
     click(By.name("update"));
   }
@@ -55,15 +48,15 @@ public class GroupHelper extends HelperBase {
     submitGroupCreation();
     returnToGroupPage();
   }
-  public void modify(int index, GroupData group) {
-   selectGroup(index);
+  public void modify(GroupData group) {
+   selectGroupById(group.getId());
    initGroupModification();
    fillGroupForm(group);
    submitGroupModification();
    returnToGroupPage();
   }
-  public void delete(int index) {
-    selectGroup(index);
+   public void delete(GroupData group) {
+    selectGroupById(group.getId());
     deleteSelectedGroups();
     returnToGroupPage();
   }
@@ -71,25 +64,24 @@ public class GroupHelper extends HelperBase {
   // проверяется наличие элемента, который хотим выбрать в selectGroup
   public boolean isThereAGroup() {
   return isElementPresent(By.name("selected[]"));
-
   }
 
   public int getGroupCount() {
      return wd.findElements(By.name("selected[]")).size();
      }
 
-  public List<GroupData> list() {
-    //создаем список, который будем заполнять
-   List<GroupData> groups=new ArrayList<GroupData>();//указываем конкретный класс, кот реализует интерфейс list
+  public Set<GroupData> all() {
+     Set<GroupData> groups=new HashSet<GroupData>();//указываем конкретный класс, кот реализует интерфейс list
     //получаем список объектоа типа WebElement
     //найти все элементы с тегом span и классoм group
-   List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));//это эл-т, внутри кот чек-бокс
-   //element пробегает по спискус elements и из каждого элемента получаем текст имя группы
-   for (WebElement element : elements){
-     String name=element.getText();
-     int  id=Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));//идентификатор, кот передаем в конструктор
-     groups.add(new GroupData().withId(id).withName(name));
-   }
-   return groups;// возвращается список groups
+    List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));//это эл-т, внутри кот чек-бокс
+    //element пробегает по спискус elements и из каждого элемента получаем текст имя группы
+    for (WebElement element : elements){
+      String name=element.getText();
+      int  id=Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));//идентификатор, кот передаем в конструктор
+      groups.add(new GroupData().withId(id).withName(name));
+    }
+    return groups;
   }
-}
+
+ }
