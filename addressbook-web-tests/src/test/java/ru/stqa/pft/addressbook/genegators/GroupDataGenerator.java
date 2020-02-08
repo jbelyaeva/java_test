b8a3e078b50a76c0b84a345abf91d7aaa38e516e
julja83 <59063644+julja83@users.noreply.github.com>
2020-02-08 14:58:15 +0300

    Добавлен генератор тестовых данных для групп, который сохраняет в файл csv

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/genegators/GroupDataGenerator.java
@@ -0,0 +1,35 @@
+package ru.stqa.pft.addressbook.genegators;
+
+import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GroupDataGenerator {
+  public static void main(String[] args) throws IOException {//если будет исключеник, программа остановится
+   int count= Integer.parseInt(args[0]);
+   File file =new File(args[1]);
+
+    List<GroupData> groups= generateGroups(count);
+    save(groups,file);
+  }
+  private static void save(List<GroupData> groups, File file) throws IOException {// если возникнет исключение - выбросить его в main
+    System.out.println(new File(".").getAbsolutePath());
+    Writer writer=new FileWriter(file);//открываем файл на запись
+    for(GroupData group : groups){
+     writer.write(String.format("%s;%s;%s\n",group.getName(), group.getHeader(), group.getFooter()));
+    }
+    writer.close();//закрыть файл
+  }
+  private static List<GroupData> generateGroups(int count) {
+    List<GroupData> groups= new ArrayList<GroupData>();
+    for (int i=0; i<count; i++){
+      groups.add(new GroupData().withName(String.format("test %s",i)).withHeader(String.format("header %s",i)).withFooter(String.format("footer %s",i)));
+    }
+    return groups;
+  }
+}
