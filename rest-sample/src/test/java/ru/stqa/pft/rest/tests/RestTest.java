eab7b80312ce567e443e8f881b1408b30666d7b4
julja83 <59063644+julja83@users.noreply.github.com>
2020-03-03 16:51:38 +0300

    Rest

--- rest-sample/src/test/java/ru/stqa/pft/rest/tests/RestTest.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/tests/RestTest.java
@@ -20,15 +20,15 @@ public class RestTest {
   @Test
   public void testCreateIssue() throws IOException {
     Set<Issue> oldIssues=getIssues();
-    Issue newIssue=new Issue().withSubject("Test issue").withDescription("New test issue");
+    Issue newIssue=new Issue().withSubject("Test issue111111111111111").withDescription("New test issue");
     int issueId = createIssue(newIssue);
     Set<Issue> newIssues=getIssues();
     oldIssues.add(newIssue.withId(issueId));
-    assertEquals(newIssues, oldIssues);
+   assertEquals(newIssues, oldIssues);
   }
 
   private Set<Issue> getIssues() throws IOException {
-    String json = getExecutor().execute(Request.Get("https://bugify.stqa.ru/api/issues.json")).returnContent().asString();
+    String json = getExecutor().execute(Request.Get("https://bugify.stqa.ru/api/issues.json?limit=1000")).returnContent().asString();
     JsonElement parsed = new JsonParser().parse(json);
     JsonElement issues = parsed.getAsJsonObject().get("issues");
     return new Gson().fromJson(issues, new TypeToken<Set<Issue>>() {}.getType());
@@ -39,9 +39,8 @@ public class RestTest {
     return Executor.newInstance().auth("288f44776e7bec4bf44fdfeb1e646490", "");
   }
 
-  private int createIssue(Issue issue) throws IOException {
-    Issue newIssue = new Issue();
-    String json = getExecutor().execute(Request.Post("https://bugify.stqa.ru/api/issues.json")
+  private int createIssue(Issue newIssue) throws IOException {
+    String json = getExecutor().execute(Request.Post("https://bugify.stqa.ru/api/issues.json?limit=1000")
             .bodyForm(new BasicNameValuePair("subject", newIssue.getSubject()),
                     new BasicNameValuePair("description", newIssue.getDescription()))).returnContent().asString();
     JsonElement parsed = new JsonParser().parse(json);//анализируем строчку
